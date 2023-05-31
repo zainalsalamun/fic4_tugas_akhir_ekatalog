a4f7df8ae118752e40a976e2a26ac1b3712dd937
Zainal Salamun <zainalrtf@gmail.com>
2023-05-31 11:34:50 +0700

    fix bug data source

--- lib/data/datasources/auth_datasources.dart
+++ lib/data/datasources/auth_datasources.dart
@@ -1,6 +1,7 @@
 import 'package:fic4_tugas_akhir_ekatalog/data/models/response/login_response_model.dart';
 import 'package:fic4_tugas_akhir_ekatalog/data/models/response/profile_response_model.dart';
 
+import '../localsources/auth_local_storage.dart';
 import '../models/request/login_model.dart';
 import '../models/request/register_model.dart';
 import '../models/response/register_response_model.dart';
@@ -19,7 +20,7 @@ class AuthDatasource {
 
   Future<LoginResponseModel> login(LoginModel loginModel) async {
     final response = await http.post(
-      Uri.parse('https://api.escuelajs.co/api/v1/auth/profile'),
+      Uri.parse('https://api.escuelajs.co/api/v1/auth/login'),
       body: loginModel.toMap(),
     );
 
@@ -28,9 +29,11 @@ class AuthDatasource {
   }
 
   Future<ProfileResponseModel> getProfile() async {
-    //final token = await AuthLocalStorage().getToken();
+    final token = await AuthLocalStorage().getToken();
+    var header = {'Authorization': 'Bearer $token'};
     final response = await http.get(
-      Uri.parse('uri'),
+      Uri.parse('https://api.escuelajs.co/api/v1/auth/profile'),
+      headers: header,
     );
 
     final result = ProfileResponseModel.fromJson(response.body);
